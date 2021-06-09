8218fcaec9d6a7418adb2f6ea2a89a4cb1c6efef
OM Miles <miles@ommiles.com>
2021-06-09 15:34:14 -0700

    lab complete

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+# name cannot be blank -- presence
+# email is unique -- uniqueness
+
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+# title cannot be blank -- presence
+# category either Fiction or Non-Fiction
+# content -- mimimun 100 chars
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category." }
 end
